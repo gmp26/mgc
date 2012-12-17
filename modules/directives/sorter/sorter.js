c5886d8493cea137061c8a0b7b1b02e2eb51e280
gmp26 <gmp26@cam.ac.uk>
2012-12-17 17:52:44 +0000

    first tests with deletes

--- modules/directives/sorter/sorter.js
+++
@@ -1,6 +0,0 @@
-angular.module('ui.directives').directive('uiSorter', ['ui.config', function () {
-  return {
-    restrict: 'EAC', // supports using directive as element, attribute and class
-		scope: true
-  };
-}]);
